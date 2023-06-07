b78dcf0a165d0c0cf604a6ac0754425d8992a348
sediri-code <63328434+sediri-code@users.noreply.github.com>
2023-06-07 19:36:59 +0200

    ##

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
 // API endpoint and API key
-const apiUrl = 'https://easysmmpanel.com/api/v2';
-const apiKey = '8a2e1bbee6246549051e5b0956a975a5';
+const apiUrl = 'https://app.ad4tube.com/api/v2';
+const apiKey = 'pYniZBZci6DcIkye31Eyjy5Sz2wgBvJ7';
 
-async function getServices() {
+async function fetchServices() {
     try {
-        const response = await fetch(`${apiUrl}?key=${apiKey}&action=services`);
-        const data = await response.json();
-
-        // Process the data and update the HTML
-        const servicesContainer = document.getElementById('services');
-        servicesContainer.innerHTML = '<h2>Services:</h2>';
-
-        data.forEach(service => {
-            const serviceElement = document.createElement('div');
-            serviceElement.classList.add('service');
-            serviceElement.innerHTML = `
-                <h3>${service.name}</h3>
-                <p>Type: ${service.type}</p>
-                <p>Category: ${service.category}</p>
-                <p>Rate: ${service.rate}</p>
-                <p>Min: ${service.min}</p>
-                <p>Max: ${service.max}</p>
-                <p>Refill: ${service.refill ? 'Yes' : 'No'}</p>
-                <p>Cancel: ${service.cancel ? 'Yes' : 'No'}</p>
-            `;
-            servicesContainer.appendChild(serviceElement);
-        });
+      const response = await fetch(`${apiUrl}?key=${apiKey}&action=services`);
+      const data = await response.json();
+  
+      const servicesContainer = document.getElementById('services');
+  
+      data.forEach(service => {
+        const serviceElement = document.createElement('div');
+        serviceElement.classList.add('service');
+  
+        const serviceName = document.createElement('h3');
+        serviceName.textContent = service.name;
+  
+        const serviceCategory = document.createElement('p');
+        serviceCategory.textContent = `Category: ${service.category}`;
+  
+        const serviceRate = document.createElement('p');
+        serviceRate.textContent = `Rate: ${service.rate}`;
+  
+        const serviceMin = document.createElement('p');
+        serviceMin.textContent = `Min: ${service.min}`;
+  
+        const serviceMax = document.createElement('p');
+        serviceMax.textContent = `Max: ${service.max}`;
+  
+        serviceElement.appendChild(serviceName);
+        serviceElement.appendChild(serviceCategory);
+        serviceElement.appendChild(serviceRate);
+        serviceElement.appendChild(serviceMin);
+        serviceElement.appendChild(serviceMax);
+  
+        servicesContainer.appendChild(serviceElement);
+      });
     } catch (error) {
-        console.error('Error retrieving services:', error);
+      console.error('Error fetching services:', error);
     }
-}
-
-// Call the getServices function to retrieve and display the list of services
-getServices();
+  }
+  
+  fetchServices();
+  
+  
